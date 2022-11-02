69dd2b4c466802b24ae0f55dc870156234d5662a
bvlavansalivahan <117214648+bvlavansalivahan@users.noreply.github.com>
2022-11-02 11:26:27 +0530

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,25 @@
+function springBtn1() {
+    x=document.getElementById("seasonSmallImage");
+    x.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+    y=document.getElementById("seasonMediumImage");
+    y.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+}
+
+function springBtn2() {
+    x=document.getElementById("seasonSmallImage");
+    x.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+    y=document.getElementById("seasonMediumImage");
+    y.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+}
+function springBtn3() {
+    x=document.getElementById("seasonSmallImage");
+    x.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+    y=document.getElementById("seasonMediumImage");
+    y.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+}
+function springBtn1() {
+    x=document.getElementById("seasonSmallImage");
+    x.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+    y=document.getElementById("seasonMediumImage");
+    y.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+}
